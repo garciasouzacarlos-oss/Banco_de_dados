549e3e5528bdea0a538ea694358725ef8ef933b3
garciasouzacarlos-oss <garcia.souza.carlos@escola.pr.gov.br>
2026-09-24 10:59:35 -0300

    Add files via upload

--- Downloads.sql
+++ Downloads.sql
@@ -34,3 +34,24 @@ ativo bool,
 foreign key (endereco_id) references enderecos(endereco_id),
 foreign key (proprietario_id) references proprietario(proprietario_id)
 );
+
+create table alugueis(
+aluguel_id varchar(255) primary key,
+cliente_id varchar(255),
+hospedagem_id varchar(255),
+data_inicio date,
+data_fin date,
+preco_total decimal(10,2),
+foreign key (cliente_id) references cliente (cliente_id),
+foreign key hospedagem (hospedagem_id) references hospedagem (hospedagem_id)
+);
+
+create table avaliacoes (
+avaliacao varchar(255) primary key,
+cliente_id varchar(255),
+hospedagem_id varchar(255),
+nata INT,
+comentario text,
+foreign key (cliente_id) references cliente(cliente_id),
+foreign key hospedagem (hospedagem_id) references hospedagem (hospedagem_id)
+);
